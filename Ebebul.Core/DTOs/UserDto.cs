4773a24c0291e0a31d51eef2813690a18b9e42f8
Nurullah Guguk <76129770+nurullahguguk@users.noreply.github.com>
2022-11-30 01:02:15 +0300

    DTOs folder is done.

---
+++ Ebebul.Core/DTOs/UserDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ebebul.Core.DTOs
+{
+    public  class UserDto : BaseDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string IdentityNum { get; set; }
+        public int Gender { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Birthplace { get; set; }
+        public int Length { get; set; }
+        public int Weight { get; set; }
+        public string Location { get; set; }
+        public string Password { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
